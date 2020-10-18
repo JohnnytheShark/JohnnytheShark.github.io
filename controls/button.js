f4aea2c4a9bc23df16bec0477e1110c4efa51742
TheSharkJO <Johnnyorellana32@gmail.com>
2020-10-18 10:25:54 -0400

    Adding Structure

---
+++ controls/button.js
@@ -0,0 +1,9 @@
+const pageSelector = Page =>{
+    if(Page === null || undefined){
+        return;
+    }
+    if(Page === 'HTML'){
+        window.location('Pages/HyperTextMarkupLanguage.html')
+    }
+    else
+}
