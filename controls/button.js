b316981a0c3407897b8720be33261e4ef479b43c
JohnnytheShark <orellanajonathan7@gmail.com>
2022-08-03 09:54:33 -0400

    Removing Unwanted Files

--- controls/button.js
+++
@@ -1,9 +0,0 @@
-const pageSelector = Page =>{
-    if(Page === null || undefined){
-        return;
-    }
-    if(Page === 'HTML'){
-        window.location('Pages/HyperTextMarkupLanguage.html')
-    }
-    else
-}
