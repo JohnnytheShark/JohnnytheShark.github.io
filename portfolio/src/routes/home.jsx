03f00f46782dae0556cff6412aba9c2ec3823bd7
JohnnytheShark <orellanajonathan7@gmail.com>
2024-03-12 12:07:53 -0400

    Updated Portfolio Website

--- portfolio/src/routes/home.jsx
+++ portfolio/src/routes/home.jsx
@@ -1,8 +1,45 @@
+
 const Home = () => {
-    return (
+    return (<div className="Container">
             <main>
-                <h1>Underconstruction please check back at a later date for an updated version of this site.</h1>
+                <figure>
+                    <picture>
+                        <img src="./mountains.jpg" alt="snowy mountain range"/>
+                    </picture>
+                </figure>
+                <h1>Stop Wasting Time Searching: Hire a Results-Oriented Engineer Today</h1>
+                <h2>I solve complex problems with elegant code. Let&apos;s discuss how I can elevate your engineering team.</h2>
            </main>
+           <article className="calltoAction">
+            <h3>Jonathan Orellana: Building Exceptional Software Solutions</h3>
+            <figure>
+                <picture>
+                    <img src="./Portrait.jpg" alt="portfolio Image"/>
+                </picture>
+            </figure>
+            <a role="button" className="Invite Bounce" href="mailto:orellanajonathan7@gmail.com" rel="noopener noreferrer" target="_blank" >Let&apos;s build something amazing together!</a>
+           </article>
+           <article className="accomplishments">
+            <span>
+            <h1>Accomplishments</h1>
+            <ul>
+                <li>Empowered accounting department with automation: Freed up 100+ hours/month by scripting daily tasks with PowerShell, Python, & SQL, enabling them to focus on high-impact strategic work.</li>
+                <li>Modernized legacy applications: Transformed outdated .asp sites into accessible, user-friendly .NET & React web apps, boosting productivity across the board.</li>
+                <li>Streamlined SOX reporting: Automated data entry, report generation, and verification using RPA bots. Saved 100 hours/month, freeing up employees for higher-value tasks. Managed project end-to-end.</li>
+                <li>Data-driven decision making: Leveraged PowerApps surveys on SharePoint to deliver actionable insights, empowering departments with data-backed strategies.</li>
+                <li>Streamlined revenue accounting operations by 3 positions through the use of Spotfire dashboard technology and Alteryx, resulting in efficiency and saving the company $150,000 annually.</li>
+                <li>Developed and implemented an Alteryx and Python application that matched payments, resulting in the processing of an average of $15M daily.</li>
+            </ul></span>
+            <figure>
+                    <picture>
+                        <img src="./coffee.jpg" alt="snowy mountain range"/>
+                    </picture>
+                </figure>
+           </article>
+           <article>
+            <h1>Testimonials</h1>
+           </article>
+           </div>
     );
 }
 
